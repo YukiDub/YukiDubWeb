11b959ab391bd04c6eb8d2e00e65bff2e9419537
m9snikfeed <den4ic2001@gmail.com>
2021-08-01 17:24:28 +0300

    added StaffFactory

---
+++ src/database/factories/StaffFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Staff;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class StaffFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Staff::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nameEn'=>$this->faker->name,
+            'nameJp'=>$this->faker->name,
+            'nameRu'=>\Str::random(23),
+            'birthday'=>$this->faker->date(),
+            'webSite'=>\Str::random(69)
+        ];
+    }
+}
