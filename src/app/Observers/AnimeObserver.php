bbef5a498bc16add85dc7239db92ecf227409650
m9snikfeed <den4ic2001@gmail.com>
2021-08-22 21:16:32 +0300

    Record of actions in Anime is displayed in a separate history

---
+++ src/app/Observers/AnimeObserver.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Anime;
+use App\Services\HistoryService;
+
+class AnimeObserver
+{
+    private $historyService;
+
+    public function __construct()
+    {
+        $this->historyService = new HistoryService();
+    }
+
+    /**
+     * Handle the Anime "created" event.
+     *
+     * @param  \App\Models\Anime  $anime
+     * @return void
+     */
+    public function created(Anime $anime)
+    {
+        $user = \Auth::user();
+        $this->historyService
+            ->createAction('anime created', 'accepted', $user->id, $anime);
+    }
+
+    /**
+     * Handle the Anime "updated" event.
+     *
+     * @param  \App\Models\Anime  $anime
+     * @return void
+     */
+    public function updated(Anime $anime)
+    {
+        $user = \Auth::user();
+        $this->historyService
+            ->updateAction('anime updated', 'accepted', $user->id, $anime);
+    }
+
+    /**
+     * Handle the Anime "deleted" event.
+     *
+     * @param  \App\Models\Anime  $anime
+     * @return void
+     */
+    public function deleted(Anime $anime)
+    {
+        $user = \Auth::user();
+        $this->historyService
+            ->removeAction('anime removed', $anime, $user->id);
+    }
+}
