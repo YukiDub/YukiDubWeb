deaa42521abc3e5c1b5f516e33210ed2b3de43b2
Daniil Myasnikov <37457328+M9snikFeed@users.noreply.github.com>
2021-09-27 19:34:26 +0300

    Fix fix

--- src/app/Providers/AuthServiceProvider.php
+++ src/app/Providers/AuthServiceProvider.php
@@ -22,31 +22,25 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Staff::class => StaffPolicy::class,
-        Anime::class => AnimePolicy::class,
+        Anime::class => AnimePolicy::class
     ];
 
     /**
      * Register any authentication / authorization services.
      *
-     * @throws \Illuminate\Contracts\Container\BindingResolutionException
-     *
      * @return void
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
     public function boot()
     {
         $this->registerPolicies();
 
         app(AuthorizationServer::class)->enableGrantType(
-<<<<<<< HEAD
             $this->makeGoogleGrant(), Passport::tokensExpireIn(),
         );
 
         app(AuthorizationServer::class)->enableGrantType(
             $this->makeVkontakteGrant(), Passport::tokensExpireIn()
-=======
-            $this->makeGoogleGrant(),
-            Passport::tokensExpireIn()
->>>>>>> b5ff7be4efed93c02586ea0bcf1623f32daae17e
         );
 
         Passport::routes();
@@ -57,16 +51,15 @@ class AuthServiceProvider extends ServiceProvider
         Passport::personalAccessTokensExpireIn(now()->addMonth(2));
 
         Passport::tokensCan([
-            'profile-management'=> 'profile management',
+            'profile-management'=>'profile management'
         ]);
     }
 
     /**
      * Create and configure a Google grant instance.
      *
-     * @throws \Illuminate\Contracts\Container\BindingResolutionException
-     *
      * @return GoogleGrant
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
     protected function makeGoogleGrant()
     {
