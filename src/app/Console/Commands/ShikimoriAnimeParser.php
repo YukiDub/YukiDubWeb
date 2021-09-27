c6e1b4618db3305378df844d6789945263ae56d4
Daniil Myasnikov <37457328+M9snikFeed@users.noreply.github.com>
2021-09-27 19:32:38 +0300

    Fix fix

--- src/app/Console/Commands/ShikimoriAnimeParser.php
+++ src/app/Console/Commands/ShikimoriAnimeParser.php
@@ -37,71 +37,63 @@ class ShikimoriAnimeParser extends Command
     public function handle()
     {
         $shikiService = new ShikimoriService();
-<<<<<<< HEAD
         for ($id = 1; $id <= 400; $id++){
             try{
-=======
-        for ($id = 1; $id <= 50000; $id++) {
-            try {
->>>>>>> b5ff7be4efed93c02586ea0bcf1623f32daae17e
                 $animeParse = $shikiService->getAnimeById($id);
                 $this->alert('parsing new anime');
                 $score = Score::create();
 
-                $imageOriginalPath = 'https://shikimori.one'.$animeParse['image']['original'];
-                $imagePreviewPath = 'https://shikimori.one'.$animeParse['image']['preview'];
-                $imageX96Path = 'https://shikimori.one'.$animeParse['image']['x96'];
-                $imageX48nalPath = 'https://shikimori.one'.$animeParse['image']['x48'];
+                $imageOriginalPath = 'https://shikimori.one' . $animeParse['image']['original'];
+                $imagePreviewPath = 'https://shikimori.one' . $animeParse['image']['preview'];
+                $imageX96Path = 'https://shikimori.one' . $animeParse['image']['x96'];
+                $imageX48nalPath = 'https://shikimori.one' . $animeParse['image']['x48'];
 
                 $anime = Anime::create([
-                    'mal_id'           => $animeParse['id'],
-                    'shiki_score'      => $animeParse['score'],
-                    'poster_original'  => ImageService::remoteUpload($imageOriginalPath, 'images/animes/original'),
-                    'poster_preview'   => ImageService::remoteUpload($imagePreviewPath, 'images/animes/preview'),
-                    'poster_x96'       => ImageService::remoteUpload($imageX96Path, 'images/animes/x96'),
-                    'poster_x48'       => ImageService::remoteUpload($imageX48nalPath, 'images/animes/x48'),
-                    'type'             => $animeParse['kind'],
-                    'episodes'         => (int) $animeParse['episodes'] ? $animeParse['episodes'] : 0,
-                    'episodes_released'=> (int)
+                    'mal_id'=>$animeParse['id'],
+                    'shiki_score'=>$animeParse['score'],
+                    "poster_original"=>ImageService::remoteUpload($imageOriginalPath, 'images/animes/original'),
+                    "poster_preview"=>ImageService::remoteUpload($imagePreviewPath, 'images/animes/preview'),
+                    "poster_x96"=>ImageService::remoteUpload($imageX96Path, 'images/animes/x96'),
+                    "poster_x48"=>ImageService::remoteUpload($imageX48nalPath, 'images/animes/x48'),
+                    "type"=>$animeParse['kind'],
+                    "episodes"=>(int)$animeParse['episodes'] ? $animeParse['episodes'] : 0,
+                    "episodes_released"=>(int)
                           $animeParse['episodes_aired'] ? $animeParse['episodes_aired']
                         : ($animeParse['status'] == 'released' ? $animeParse['episodes'] : 0),
-                    'next_episode_at'      => null,
-                    'episode_duration'     => $animeParse['duration'] ? date('G:i:s', mktime(0, $animeParse['duration'], 0)) : null,
-                    'status'               => $animeParse['status'],
-                    'aired_on'             => (int) $animeParse['aired_on'] ? $animeParse['aired_on'] : null,
-                    'released_on'          => (int) $animeParse['released_on'] ? $animeParse['released_on'] : null,
-                    'age_rating'           => $animeParse['rating'] ?: null,
-                    'name_jp'              => $animeParse['japanese'][0],
-                    'name_en'              => $animeParse['name'],
-                    'name_ru'              => $animeParse['russian'] ?: null,
-                    'description_ru'       => $animeParse['description'] ?: null,
-                    'description_ru_source'=> 'https://shikimori.one/animes/'.$id,
-                    'score'                => $score->score_id,
+                    "next_episode_at"=>null,
+                    "episode_duration"=>$animeParse['duration'] ? date('G:i:s', mktime(0, $animeParse['duration'], 0)) : null,
+                    "status"=>$animeParse['status'],
+                    "aired_on"=> (int)$animeParse['aired_on'] ? $animeParse['aired_on'] : null,
+                    "released_on"=> (int)$animeParse['released_on'] ? $animeParse['released_on'] : null,
+                    "age_rating"=> $animeParse['rating'] ?: null,
+                    "name_jp"=>$animeParse['japanese'][0],
+                    "name_en"=>$animeParse['name'],
+                    "name_ru"=> $animeParse['russian'] ?: null,
+                    "description_ru"=>$animeParse['description'] ?: null,
+                    "description_ru_source"=>'https://shikimori.one/animes/' . $id,
+                    'score'=>$score->score_id
                 ]);
 
-                foreach ($animeParse['studios'] as $studioItem) {
-                    if (empty($studio = AnimeStudio::where('name', '=', $studioItem['name'])->first())) {
+                foreach ($animeParse['studios'] as $studioItem){
+                    if(empty($studio = AnimeStudio::where('name', '=', $studioItem['name'])->first())){
                         $studio = AnimeStudio::create(['name'=>$studioItem['name']]);
                     }
                     $anime->studios()->attach($studio);
                 }
 
-                foreach ($animeParse['genres'] as $genreItem) {
+                foreach ($animeParse['genres'] as $genreItem){
                     $genre = Genre::where('nameEn', '=', $genreItem['name'])->first();
                     $anime->genres()->attach($genre);
                 }
-            } catch (QueryException $queryException) {
-            } catch (ShikimoriException $shikiEx) {
+            }
+           catch (QueryException $queryException){
+
+           }
+           catch (ShikimoriException $shikiEx){
                 Log::emergency($shikiEx);
-<<<<<<< HEAD
            }
                 $this->alert("sleeping");
                 sleep(rand(20,60));
-=======
-            }
-            $this->alert('sleeping');
-            sleep(rand(40, 120));
->>>>>>> b5ff7be4efed93c02586ea0bcf1623f32daae17e
         }
     }
 }
