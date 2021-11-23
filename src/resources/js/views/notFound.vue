8a106901122d706e5bacd10d1eda1dfedf3e48d2
Мясников Даниил <37457328+M9snikFeed@users.noreply.github.com>
2021-11-23 18:54:42 +0300

    updated design on the page not found

--- src/resources/js/views/notFound.vue
+++ src/resources/js/views/notFound.vue
@@ -5,8 +5,11 @@
   -->
 
 <template>
-  <div>
-    <p class="text-center">СТРАНИЦА НЕ НАЙДЕНА</p>
+  <div class="not-found">
+    <img src="/assets/images/nya404.webp" alt="nya 404" class="img-fluid w-25 mb-4 mx-auto d-block">
+    <h1>Ошибка 404</h1>
+    <p class="text-center">Упс... Страница, которую вы запрашиваете, не существует. Возможно был введен неверный адрес.</p>
+    <router-link to="/" class="btn d-block">Вернуться на главную страницу</router-link>
   </div>
 </template>
 
